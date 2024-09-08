0f37a9e5c353c9151aa298de4efa375a981cda68
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-09-09 02:09:12 +0300

    Type Narrowing

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log('Search term:', e.target.value);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log('Button clicked, search term:', search);
+  const onClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+
+    if (typeof result === 'string') {
+      setServerError(result);
+      setSearchResult([]);
+    } else if (Array.isArray(result)) {
+      setSearchResult(result);
+      setServerError("");
+    }
+
+    console.log('Search results:', searchResult);
   };
 
   return (
@@ -23,8 +38,8 @@ function App() {
         handleChange={handleChange}
         search={search} onSearch={function (search: string): void {
           throw new Error('Function not implemented.');
-        }}
-      />
+        }} />
+      {serverError && <p style={{ color: 'red' }}>{serverError}</p>}
     </div>
   );
 }
