ffecbb0793a5c9670a599361578161b28f141bd1
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-09-20 15:50:34 +0300

    DTOs

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
-using api.Data; 
-using api.Models;
 
 namespace api.Controllers
 {
@@ -13,7 +13,6 @@ namespace api.Controllers
     public class StockController : ControllerBase
     {
         private readonly ApplicationDBContext _context;
-
         public StockController(ApplicationDBContext context)
         {
             _context = context;
@@ -22,61 +21,24 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok(_context.Stocks.ToList()); 
+            var stocks = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
+
+            return Ok(stocks);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute]int id)
+        public IActionResult GetById([FromRoute] int id)
         {
             var stock = _context.Stocks.Find(id);
-            if (stock == null)
-            {
-                return NotFound();
-            }
-            return Ok(stock);
-        }
 
-        [HttpPost]
-        public IActionResult Create(Stock stock)
-        {
-            _context.Stocks.Add(stock);
-            _context.SaveChanges();
-            return CreatedAtAction(nameof(GetById), new { id = stock.Id }, stock);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Stock updatedStock)
-        {
-            var stock = _context.Stocks.Find(id);
             if (stock == null)
             {
                 return NotFound();
             }
 
-            stock.CompanyName = updatedStock.CompanyName;
-            stock.Symbol = updatedStock.Symbol;
-            stock.Purchase = updatedStock.Purchase;
-            stock.LastDiv = updatedStock.LastDiv;
-            stock.Industry = updatedStock.Industry;
-            stock.MarketCap = updatedStock.MarketCap;
-
-            _context.Stocks.Update(stock);
-            _context.SaveChanges();
-            return NoContent();
+            return Ok(stock.ToStockDto());
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var stock = _context.Stocks.Find(id);
-            if (stock == null)
-            {
-                return NotFound();
-            }
-
-            _context.Stocks.Remove(stock);
-            _context.SaveChanges();
-            return NoContent();
-        }
     }
 }
