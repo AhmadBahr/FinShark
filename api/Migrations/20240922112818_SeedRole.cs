c5f45b2947fdbde2f925d9f7e401c7df0ce7aebf
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-09-22 14:32:11 +0300

    Register

---
+++ api/Migrations/20240922112818_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2d8fccdd-bfa3-45b9-9d04-8224d195adfb", null, "User", "USER" },
+                    { "c14635b6-2a6d-421f-a542-7e5af4d4aa78", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2d8fccdd-bfa3-45b9-9d04-8224d195adfb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c14635b6-2a6d-421f-a542-7e5af4d4aa78");
+        }
+    }
+}
