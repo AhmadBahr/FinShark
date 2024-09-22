4b9919760fb1a747f09619bf2095e681e36d88b6
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-09-23 00:58:12 +0300

    Context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Navbar from "./Components/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </>
   );
 }
+
 export default App;
